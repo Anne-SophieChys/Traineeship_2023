cc3669850b0736416028904d5eaae55fa0992c88
Anne-Sophie Chys <anne-sophie.chys@student.howest.be>
2023-05-17 11:46:50 +0200

    Created a Bash Script that loops over the directories and execute the README files

--- Execute_Dynamics.sh
+++ Execute_Dynamics.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Specify the directory to start with
-for dir in CHARMM*;
+for dir in charmm*;
 do
 	# Go into the 'amber' directory
-	cd "$dir/amber" || continue
+	cd "$dir/amber"
 	
 	# Check for the README file
 	if [ -f "README" ];
@@ -12,13 +12,14 @@ do
 		# Execute the README file
 		echo "Executing ./README"
 		./README
+		echo "Go to the next"
 		
 	else
 		echo "README file not found in $dir/amber"
 	fi
 	
 	# Go back to the parend directory
-	cd ./..
+	cd ./../..
 
 done
 	
