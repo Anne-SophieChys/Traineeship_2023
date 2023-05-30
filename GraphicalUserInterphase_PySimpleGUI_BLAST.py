1b9e6d4c7deeb8a83b30eb5bb217279c3f41dbde
Anne-Sophie Chys <anne-sophie.chys@student.howest.be>
2023-05-30 11:44:38 +0200

    The MSA, performed with https://www.ebi.ac.uk/Tools/msa/clustalo/ that works!

--- GraphicalUserInterphase_PySimpleGUI_BLAST.py
+++ GraphicalUserInterphase_PySimpleGUI_BLAST.py
@@ -70,7 +70,7 @@ soup = BeautifulSoup(data, "html.parser")
 headersclustal = {}
 headersclustal["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"
-urlclustal = "https://www.genome.jp/tools-bin/clustalw"
+urlclustal = "https://www.ebi.ac.uk/Tools/msa/clustalo/"
 
 try:
     requestclustal = urllib.request.Request(urlclustal, headers = headersclustal)
@@ -226,64 +226,42 @@ of the online Clustal Omega algorithm to perform accurate and efficient sequence
 leveraging the Clustal Omega tool, ANSO ensures that your sequences are aligned with precision, \
 considering both sequence conservation and structural compatibility.', size=(105,None))],
 
-              [sg.Text('Output Format: '), sg.Combo(values=('CLUSTAL', 'GCG(MSF)', 'GDE', 'PIR', 'Phylip', 'FASTA'), default_value='CLUSTAL', key = '-OUTPUTCLUSTAL-'),
-               sg.Text('\tType of sequence: '),
-               sg.Radio('PROTEIN', 'typeseq', key='-TYPE2SEQ-', default=True),
-               sg.Radio('DNA', 'typeseq', key='-TYPE2SEQ-')],
-              [sg.Text('\t\t\t\t       Pairwise Alignment: '),
-               sg.Radio('FAST/APPROXIMATE', 'PA', key='-FASTSLOW-', default=True),
-               sg.Radio('SLOW/ACCURATE', 'PA', key='-FASTSLOW-')],
+              [sg.Text('Type of sequence: '), 
+               sg.Combo(values=('PROTEIN', 'DNA', 'RNA'), key='-TYPE2SEQ-', default_value='PROTEIN',
+                        size=(9,0), enable_events=True)],
               [sg.Text('Enter Query Sequences', font='AnyFont 12 bold')],
-              [sg.Text('Support Formats: FASTA, NBRF/PIR, EMBL/Swiss Prot, GDE, CLUSTAL, and GCT/MSF', font='AnyFont 9 bold')],
-              [sg.Multiline(key= '-QUERYMSA-', size=(111,5))],
-              [sg.Text('Or, upload file', font='AnyFont 9 bold'), 
+              [sg.Text('Support Formats: FASTA, GCG, EMBL (Nucleotide only), GenBank, PIR/NBRF, PHILIP or UniProtKB/Swiss-Prot (Protein only)', font='AnyFont 9 bold')],
+              [sg.Multiline(key= '-QUERYMSA-', size=(111,6))],
+              [sg.Text('Or, upload file', font='AnyFont 9 bold'),
                sg.VSeparator(),
                sg.FileBrowse(key='-BROWSE-')],
               
-              # Pairwise Alignment Parameters ------------------------------------
-              [sg.Text('Pairwise Alignment Parameters', font='AnyFont 12 bold')],
-              [sg.Text('For FAST/APPROXIMATE:', font='AnyFont 10'),
-               sg.Text('\tK-tuple(word) size:'), sg.Input(size=(5,0), pad=(0,0), default_text=1, key='-KTUPLE-'),
-               sg.Text('  Window size:'), sg.Input(size=(5,0), pad=(0,0), default_text=5, key='-WINDOWSIZE-'),
-               sg.Text('  Gap Penalty:'), sg.Input(size=(5,0), pad=(0,0), default_text=3, key='-GAPPENALTY-')],
+              [sg.HSeparator()],
+              [sg.Text('Set parameters', font='AnyFont 12 bold')],
+              [sg.Text('Output Format:'), sg.Combo(values=('ClustalW with character counts', 'ClustalW',
+                                                           'Pearson/FASTA', 'MSF', 'NEXUS', 'PHYLIP',
+                                                           'SELEX', 'STOCKHOLM', 'VIENNA'),
+                                                           default_value='ClustalW with character counts',
+                                                           key = '-OUTPUTMSA-')],
 
-              [sg.Text('\t\t\t\tNumber of Top Diagonals:'), sg.Input(size=(5,0), pad=(0,0), default_text=5, key='-TOPDIAGONALS-'),
-               sg.Text('  Scoring Method:'), sg.Combo(values=('PERCENT', 'ABSOLUTE'), default_value='PERCENT',
-                                                      size=(10,0), key='-SCORINGMETHOD-')],
-              
-              [sg.Text('For SLOW/ACCURATE:', font='AnyFont 10'),
-               sg.Text('\t    Gap Open Penalty:'), sg.Input(size=(5,0), pad=(0,0), default_text=10.0, key='-GAPOPENPENALTYPA-'),
-               sg.Text('Gap Extension Penalty:'), sg.Input(size=(5,0), pad=(0,0), default_text=0.1, key='-GAPEXTENSIONPENALTYPA-')],
-               
-              [sg.Text('\t\t\t\tSelect Weight Matrix:'), sg.Combo(values=('BLOSUM (for PROTEIN)',
-                                                                  'PAM (for PROETIN)',
-                                                                  'GONNET (for PROTEIN)',
-                                                                  'ID (for PROTEIN)',
-                                                                  'IUB (for DNA)',
-                                                                  'CLUSTALW (for DNA)'),
-                                                                  default_value='BLOSUM (for PROTEIN)',
-                                                                  key='-WEIGHTMATRIXPA-')],
-              
-              # Multiple Aligmnent Parameters ------------------------------------
-              [sg.Text('Multiple Alignment Parameters', font='AnyFont 12 bold')],
-              [sg.Text('Gap Open Penalty:'), sg.Input(size=(5,0), pad=(0,0), default_text=10, key='-GAPOPENPENALTYMA-'),
-               sg.Text('Gap Extension Penalty:'), sg.Input(size=(5,0), pad=(0,0), default_text=0.05, key='-GAPEXTENSIONPENALTYMA-'),
-               sg.Text('Weight Transition:'), sg.Radio('YES (Value:', 'WeightT', key='-WEIGHTTRANSITION-'),
-               sg.Input(size=(5,0), pad=(0,0), key='-WEIGHTTRANSITIONVALUE-', default_text=0.5), sg.Text('),'),
-               sg.Radio('NO', 'WeightT', default=True, key='-WEIGHTTRANSITION-')],
+              [sg.Text('\nDealign Input Sequences     MBED-Like Clustering Guide-Tree\
+     Max HMM Iterations                       Distance Matrix')],
+              [sg.Combo(values=('yes', 'no'), default_value='no', key='-DEALIGN-', size=(20,0)),
+               sg.Combo(values=('yes', 'no'), default_value='yes', key='-MBED-', size=(27,0)),
+               sg.Combo(values=('default', 0,1,2,3,4,5), default_value='default', key='-HMMITERATIONS-', size=(26,0)),
+               sg.Combo(values=('yes', 'no'), default_value='no', key='-DISMATOUT-', size=(20,0))],
 
-               [sg.Text('Hydrophilic Residues (PROTEINS):'),
-                sg.Input(key='-HYDROPHILICRESI-', size=(12,0), pad=(0,0), default_text='GPSNDQERK'),
-                sg.Text('Hydrophilic Gaps:'),
-                sg.Radio('YES', 'HydroGaps', pad=(0,0), default=True, key='-HYDROPHILICGAPS-'),
-                sg.Radio('NO', 'HydroGaps', pad=(0,0), key='-HYDROPHILICGAPS-'),
-                sg.Text('Select Weight Matrix:'), sg.Combo(values=('BLOSUM (for PROTEIN)',
-                                                                   'PAM (for PROETIN)',
-                                                                   'GONNET (for PROTEIN)',
-                                                                   'ID (for PROTEIN)',
-                                                                   'IUB (for DNA)',
-                                                                   'CLUSTALW (for DNA)'), key='-WEIGHTMATRIXMA-')],
-              [sg.Text('\t\t\t\t\t  '),
+              [sg.Text('Max Guide Tree Iterations    MBED-Like Clustering Iteration\
+         Number of Combined Iterations       Guide Tree')],
+              [sg.Combo(values=('default', 0,1,2,3,4,5), default_value='default', key='-GTITERATIONS-' ,size=(20,0)),
+               sg.Combo(values=('yes', 'no'), default_value='yes', key='-MBEDITERATION-', size=(27,0)),
+               sg.Combo(values=('default(0)', 1,2,3,4,5), default_value='default(0)', key='-ITERATIONS-', size=(26,0)),
+               sg.Combo(values=('yes', 'no'), default_value='yes', key='-GUIDETREEOUT-', size=(20,0))],
+
+              [sg.Text('Order')],
+              [sg.Combo(values=('aligned', 'input'), default_value='aligned', key='-ORDER-', size=(20,0))],
+              
+              [sg.Text('\n\n\t\t\t\t\t  '),
                sg.Button('Execute MSA', font='AnyFont, 13')],
               [sg.Text('\t\t\t\t\t             '),
                sg.Image(data=sg.DEFAULT_BASE64_LOADING_GIF, key='-GIF1-',
@@ -530,68 +508,52 @@ while True:
         driver = (webdriver.Firefox())
         driver.get(urlclustal)
 
-        # Input information about the output format -OUTPUTCLUSTAL-
-        inputoutputclustal = values['-OUTPUTCLUSTAL-']
-        outputclustal = driver.find_element(By.NAME, "output")
-        outputclustal.clear()
-        outputclustal.send_keys(inputoutputclustal)
+        # Cookies
+        cookies = driver.find_element(By.ID, "data-protection-agree")
+        cookies.click()
 
-        # Input information about the pairwise alignment
-        driver.find_element(By.NAME, "pwalignment").send_keys(values['-FASTSLOW-'])
+        # Input information about the type of the sequence -TYPE2SEQ-
+        driver.find_element(By.ID, 'stype').send_keys(values['-TYPE2SEQ-'])
 
-        # Input information about the type of the sequence
-        driver.find_element(By.NAME, "type").send_keys(values['-TYPE2SEQ-'])
+        # Input information about the query -QUERYMSA-
+        driver.find_element(By.ID, 'sequence').send_keys(values['-QUERYMSA-'])
 
-        # Input information about the query
-        driver.find_element(By.NAME, "sequence").send_keys(values['-QUERYMSA-'])
+        # Input information about the output format -OUTPUTMSA-
+        driver.find_element(By.ID, 'outfmt').send_keys(values['-OUTPUTMSA-'])
 
-        # Input information about the K-tuple(word) size
-        driver.find_element(By.NAME, "ktuple").send_keys(values['-KTUPLE-'])
+        # Click 'more options'
+        options = driver.find_element(By.XPATH, '//*[@id="jd_toolSubmissionForm"]/div[2]/fieldset/p/a')
+        options.click()
 
-        # Input information about the window size
-        driver.find_element(By.NAME, "window").send_keys(values['-WINDOWSIZE-'])
+        # Input information about the dealign input sequence -DEALIGN-
+        driver.find_element(By.ID, 'dealign').send_keys(values['-DEALIGN-'])
 
-        # Input information about the gap penalty
-        driver.find_element(By.NAME, "pairgap").send_keys(values['-GAPPENALTY-'])
+        # Input information about the MBED-like clustering guide-tree -MBED-
+        driver.find_element(By.ID, 'mbed').send_keys(values['-MBED-'])
 
-        # Input information about the number of top diagonals
-        driver.find_element(By.NAME, "topdiags").send_keys(values['-TOPDIAGONALS-'])
+        # Input information about the max HMM iterations -HMMITERATIONS-
+        driver.find_element(By.ID, 'hmmiterations').send_keys(values['-HMMITERATIONS-'])
 
-        # Input information about the scoring method
-        driver.find_element(By.NAME, "score").send_keys(values['-SCORINGMETHOD-'])
+        # Input information about the distance matrix -DISMATOUT-
+        driver.find_element(By.ID, 'dismatout').send_keys(values['-DISMATOUT-'])
 
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "pwgapopen").send_keys(values['-GAPOPENPENALTYPA-'])
+        # Input information about the max guide tree iterations -GTITERATIONS-
+        driver.find_element(By.ID, 'gtiterations').send_keys(values['-GTITERATIONS-'])
 
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "pwgapext").send_keys(values['-GAPEXTENSIONPENALTYPA-'])
+        # Input information about the MBED-like clustering iteration -MBEDITERATION-
+        driver.find_element(By.ID, 'mbediteration').send_keys(values['-MBEDITERATION-'])
 
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "pwmatrix").send_keys(values['-WEIGHTMATRIXPA-'])
+        # Input information about the number of combined iterations -ITERATIONS-
+        driver.find_element(By.ID, 'iterations').send_keys(values['-ITERATIONS-'])
 
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "gapopen").send_keys(values['-GAPOPENPENALTYMA-'])
+        # Input information about the number of the guide tree -GUIDETREEOUT-
+        driver.find_element(By.ID, 'guidetreeout').send_keys(values['-GUIDETREEOUT-'])
 
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "gapext").send_keys(values['-GAPEXTENSIONPENALTYMA-'])
-
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "transitions").send_keys(values['-WEIGHTTRANSITION-'])
-        
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "trans_weight").send_keys(values['-WEIGHTTRANSITIONVALUE-'])
-
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "hgapresidues").send_keys(values['-HYDROPHILICRESI-'])
-
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "nohgap").send_keys(values['-HYDROPHILICGAPS-'])
-
-        # Input information about the gap open penalty
-        driver.find_element(By.NAME, "matrix").send_keys(values['-WEIGHTMATRIXMA-'])
+        # Input information about the number of the order -ORDER-
+        driver.find_element(By.ID, 'order').send_keys(values['-ORDER-'])
 
         # Click on the Execute Multiple Aglinment Button
-        ExecuteMSA = driver.find_element(By.XPATH, '//*[@id="clustalw"]/input[4]')
+        ExecuteMSA = driver.find_element(By.XPATH, '//*[@id="jd_submitButtonPanel"]/input')
         ExecuteMSA.click()
         print('[LOG] MSA has started')
 
